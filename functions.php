f6da3837d96e6ba48e1fb2a771abbd09aa5641df
dibuchis <dibuchis@gmail.com>
2019-05-27 14:46:16 -0500

    First Commit

---
+++ functions.php
@@ -0,0 +1,485 @@
+<?php
+
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'avada-stylesheet' ) );
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+function avada_lang_setup() {
+	$lang = get_stylesheet_directory() . '/languages';
+	load_child_theme_textdomain( 'Avada', $lang );
+}
+add_action( 'after_setup_theme', 'avada_lang_setup' );
+
+/**
+ *
+ * API - YouNeed
+ * INICIAR SESSION HANDLER
+ * 
+ */
+add_action('init', 'youneed_session_start', 1);
+function youneed_session_start() {
+    if( ! session_id() ) {
+        session_start();
+    }
+}
+
+/**
+ *
+ * API - YouNeed
+ * CUSTOM QUERY VARS
+ * 
+ */
+function custom_query_vars_filter($vars) {
+  $vars[] .= 'cat_id';
+  $vars[] .= 'srv_id';
+  $vars[] .= 'id';
+  $vars[] .= 'page';
+  return $vars;
+}
+add_filter( 'query_vars', 'custom_query_vars_filter' );
+
+/**
+ *
+ * API - YouNeed
+ * OBTENER CATEGORIAS
+ * 
+ */
+function api_youneed_categorias(){
+    // wp_register_style('my_stylesheet_1', 'https://youneed.com.ec/app/css/owl.carousel.min.css');
+    // wp_register_style('my_stylesheet_2', 'https://youneed.com.ec/app/css/owl.theme.default.min.css');
+    // wp_register_script('owl-carrousel-api', 'https://youneed.com.ec/app/js/owl.carousel.min.js', array('jquery'),'1.1', true);
+    wp_register_script('load-owl-carrousel-api', 'https://youneed.com.ec/app/js/loadCarousel.js', array('jquery'),'1.1', true);
+    
+    // wp_enqueue_style('my_stylesheet_1');
+    // wp_enqueue_style('my_stylesheet_2');
+    // wp_enqueue_script('owl-carrousel-api');
+    
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, 'https://app.youneed.com.ec/ajax/listadocategorias');
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+     
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+     
+    $data = curl_exec($ch);
+
+    curl_close($ch);
+
+    $result = json_decode($data);
+    
+    $cats = $result->output;
+    
+    $out = '<i>No se han encontrado categorias disponibles.</i>';
+    
+    if($cats){
+        
+        $out = '<div class="owl-carousel owl-theme owl-api swiper-container" id="categorias-youneed">';    
+        
+        $out .= '<div class="owl-carousel owl-theme owl-api swiper-wrapper hidden">';    
+        
+        foreach ($cats as $key => $value) {
+           $out .= '<div class="item item-categoria swiper-slide swiper-lazy">';
+                $out .= '<a href="/servicios?cat_id=' . $value->id . '"><img src="' . $value->imagen . '" alt="' . $value->nombre . '"/></a>';    
+                $out .= '<center><span class="cat-text">' . $value->nombre . '</span></center>';    
+            $out .= '</div>';
+              
+        }
+      
+        $out .= '</div>';
+        
+        $out .= '<div class="swiper-lazy-preloader"></div>';
+        
+
+        $out .= '<div class="swiper-pagination"></div>';
+
+        $out .= '<div class="swiper-button-next"></div>';
+        $out .= '<div class="swiper-button-prev"></div>';
+      
+        
+        $out .= '</div>';
+    }else{
+        
+    }
+    
+    //var_dump($cats);
+    
+    
+    wp_enqueue_script('load-owl-carrousel-api');
+    
+    return $out;
+    
+}
+add_shortcode( 'api_youneed_categorias', 'api_youneed_categorias' );
+
+
+/**
+ *
+ * API - YouNeed
+ * OBTENER SERVICIOS POR CATEGORIA ID
+ * 
+ */
+function api_youneed_servicios(){
+    
+    if($_REQUEST['cat_id']){
+        $cat_id = $_REQUEST['cat_id'];
+    }else{
+        $cat_id = 1;
+    }
+    
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, 'https://app.youneed.com.ec/ajax/listadoservicios?depdrop_parents=' . $cat_id);
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+     
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+     
+    $data = curl_exec($ch);
+
+    curl_close($ch);
+
+    $result = json_decode($data);
+    
+    $cats = $result->output;
+    
+    $out = '<i>No se han encontrado servicios disponibles en esta categoría.</i>';
+    
+    if($cats){
+        
+        $out = '<div class="catalogo-servicios" id="servicios-youneed">';    
+        $out .= '<h2>Listado de Servicios</h2>';  
+        
+        foreach ($cats as $key => $value) {
+           $out .= '<div class="item item-servicio">';
+               $out .= '<div class="item-inner">';
+                    $out .= '<div class="item-expand">';
+                        $out .= '<div class="img-panel meta-imagen"><img src="' . $value->imagen . '" alt="' . $value->nombre . '"/></div>';    
+                        $out .= '<div class="content-panel meta-content">';
+                            $out .= '<div class="meta meta-nombre">' . $value->nombre  . '</div>';
+                            //$out .= '<label><b>Precio: </b></label><div class="meta meta-precio">' . $value->precio  . '</div>';
+                            $out .= '<div class="meta meta-desc"><a class="ver-detalles link-servicio" onclick="getServicio(' . $value->id . ')">Ver Detalles</a></div>';
+                            $out .= '<div class="meta meta-link"><a class="ver-asociados btn-asociados" href="/servicio-asociados?srv_id=' . $value->id . '">Ver Profesionales</a></div>';
+                        $out .= '</div>';
+                    $out .= '</div>';
+                $out .= '</div>';
+            $out .= '</div>';
+        }
+      
+        $out .= '</div>';
+    }else{
+        
+    }
+    
+    //var_dump($cats);
+    
+    return $out;
+    
+}
+
+add_shortcode( 'api_youneed_servicios', 'api_youneed_servicios' );
+
+
+/**
+ *
+ * API - YouNeed
+ * OBTENER LISTA DE ASOCIADOS
+ * 
+ */
+function api_youneed_asociados(){
+    
+	
+	
+    if($_REQUEST['srv_id']){
+        $srv_id = $_REQUEST['srv_id'];
+		$_SESSION['servicio_id'] = $_REQUEST['srv_id'];
+    }else{
+        $srv_id = 1;
+    }
+	
+	$chUrl = 'https://app.youneed.com.ec/ajax/listadoasociados?srv_id=' . $srv_id;
+	
+	$nPage = get_query_var('page');
+	
+	if($nPage > 1){
+		$chUrl = 'https://app.youneed.com.ec/ajax/listadoasociados?srv_id=' . $srv_id . "&page=" . $nPage;
+	}
+    
+    $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $chUrl);
+    
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+         
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+         
+        $data = curl_exec($ch);
+    
+        curl_close($ch);
+    
+        $result = json_decode($data);
+		
+		// echo "<pre>";
+		// var_dump($result->total);
+		// var_dump($result->pages);
+		// var_dump($result->offset);
+		// var_dump($result->rows);
+		// echo "</pre>";
+		
+        $asoc = $result->output;
+    
+    $out = '<i>No se han encontrado asociados disponibles para este servicio.</i>';
+    
+    if($asoc){
+        
+        $out = '<div class="catalogo-asociados" id="asociados-youneed">';    
+        //$out .= '<h2>Listado de Servicios</h2>';  
+        
+        foreach ($asoc as $key => $value) {
+			$params = array('id' => $value->id, 'srv_id' => $srv_id);
+            $out .= '<div class="item item-asociado">';
+                $out .= '<div class="left-panel meta-imagen"><img src="' . $value->imagen . '" alt="' . $value->nombre . '"/></div>';    
+                $out .= '<div class="right-panel meta-content">';
+                    $out .= '<div class="meta meta-nombre-asociado">' . strtolower($value->nombre)  . '</div>';
+                    $out .= '<div class="meta meta-rating"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i></div>';
+                    $out .= '<div class="meta meta-ciudad">' . $value->ciudad->nombre  . '</div>';
+                    //$out .= '<label><b>Precio: </b></label><div class="meta meta-precio">' . $value->precio  . '</div>';
+                    $out .= '<div class="meta meta-link"><a class="ver-asociados btn-asociados" href="' . add_query_arg($params, '/ver-asociado') . '">Ver Perfil</a></div>';
+					///ver-asociado?id=' . $value->id . '
+                $out .= '</div>';
+            $out .= '</div>';
+            $out .= '<hr class="hr-asoc">';
+              
+        }
+		
+		if($result->pages > 1){
+			$out .= '<div class="pagination-asoc">';
+
+				for($i = 1; $i <= $result->pages; $i++){
+
+					$params = array('srv_id' => $srv_id, 'page' => $i);
+					$out .= '<div class="page-asoc">';
+						
+						if($i != $nPage){
+							$out .= '<a class="page-link" href="' . add_query_arg($params, '/servicio-asociados') . '" >' . $i . '</a>';
+						}else{
+							$out .= '<span class="page-link">' . $i . '</span>';	
+						}
+					
+					$out .= '</div>';
+				
+				}
+			$out .= '</div>';
+		}
+	  
+        $out .= '</div>';
+    }else{
+        
+    }
+    
+    //var_dump($cats);
+    
+    return $out;
+    
+}
+
+add_shortcode( 'api_youneed_asociados', 'api_youneed_asociados' );
+
+
+/**
+ *
+ * API - YouNeed
+ * CONTAR ASOCIADOS SEGUN CATEGORIA
+ * 
+ */
+function api_youneed_contar_asociados(){
+    
+    $out = '';
+    
+    if($_REQUEST['srv_id']){
+        $srv_id = $_REQUEST['srv_id'];
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, 'https://app.youneed.com.ec/ajax/contarasociados?srv_id=' . $srv_id);
+    
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+         
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+         
+        $data = curl_exec($ch);
+    
+        curl_close($ch);
+        
+        $result = json_decode($data);
+        
+        $text = "<h3 class='filtro-titulo'><b>" . $result->nombre_servicio . "</b></h3>"; 
+        $text .= "<span class='filtro'>" . $result->count . ($data > 1 ? " resultados" : " resultado") . "</span>";
+        
+        return $text;
+    }else{
+        return 0;
+    }
+}
+add_shortcode( 'api_youneed_contar_asociados', 'api_youneed_contar_asociados' );
+
+
+/**
+ *
+ * API - YouNeed
+ * MOSTRAR DATOS ASOCIADO
+ * 
+ */
+function api_youneed_asociado(){
+    
+    $user = null;
+
+    if(isset($_SESSION["api_userdata"])) {
+        $data = $_SESSION["api_userdata"];
+        $user = json_encode($data);
+        //echo "USAURIO";
+    }
+    
+	$srv_id = get_query_var('srv_id');
+    
+    if($user){
+        //echo $data->usuario;
+        //echo json_encode($data);
+        
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, 'https://app.youneed.com.ec/ajax/verasociado?aso_id=' . $_REQUEST['id'] . '&api_token=8e705fdb6ed22df72e4fcbeb37bcf517');
+    
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+         
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+         
+        $dataAsoc = curl_exec($ch);
+    
+        curl_close($ch);
+        
+        $asociado = json_decode($dataAsoc);
+        
+        $dias = [ 1 => 'Lunes a Viernes', 2 => 'Fines de semana', 3 => 'Cualquier día' ];
+        $horarios = [ 1 => '7am a 12 am', 2 => '12am a 7pm', 3 => '7pm a 7 am', 4 => '24 horas' ];
+        
+		$srv_id = get_query_var('srv_id');
+		
+		if(!($srv_id > 0)){
+			$srv_id = $_SESSION['servicio_id'];
+		}
+        
+        $out = '<div id="panel-asociado" class="fusion-fullwidth fullwidth-box hundred-percent-fullwidth non-hundred-percent-height-scrolling" style="background-color: #f3f3f3;background-position: center center;background-repeat: no-repeat;padding-top:45px;padding-right:8%;padding-bottom:45px;padding-left:8%;"><div class="fusion-builder-row fusion-row "><div class="fusion-layout-column fusion_builder_column fusion_builder_column_1_1 fusion-builder-column-2 fusion-one-full fusion-column-first fusion-column-last 1_1" style="margin-top:0px;margin-bottom:20px;">';
+        $out .= '<h2>Asociado</h2>';
+        $out .= '<hr>';
+        $out .= '<div class="panel-asociado">';
+        $out .= '<form id="contratar-asociado">';
+			//$out .= '<input type="hidden" name="_csrf" value="XDB8ErUw8zD_28OF8uOJGeVszR7GuztlpYlXhhaPVNYTWDlcgFW_QZmR7rynishGig2scrH4Yg_20RnBL7cVsg==">';
+			$out .= '<input id="asociado_id" type="hidden" name="asociado_id" value="' . $asociado->id . '">';
+			$out .= '<input id="cliente_id" type="hidden" name="cliente_id" value="' . $data->usuario->id . '">';
+			$out .= '<input id="servicio_id" type="hidden" name="servicio_id" value="' . $srv_id . '">';
+            $out .= '<div class="left-panel">';
+                $out .= '<img class="asociado-vista-img" src="' . $asociado->imagen . '">';
+            $out .= '</div>';
+            
+            $out .= '<div class="right-panel">';
+                
+                $out .= '<label><b>Nombre</b></label>';
+                $out .= '<p><span>' . ucwords(strtolower($asociado->nombres)) . " " . ucwords(strtolower($asociado->apellidos)) . '</span></p>';
+                
+                $out .= '<div class="meta meta-rating"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i></div>';
+                
+                $out .= '<label><b>Estado</b></label>';
+                $out .= '<p><span>' . $asociado->estado . '</span></p>';
+                
+                
+                //$out .= '<h4>Disponibilidad</h4>';
+
+                $out .= '<div class="fusion-builder-row fusion-row">';
+                
+                    $out .= '<div class="fusion-layout-column fusion_builder_column fusion_builder_column_1_2 fusion-builder-column-7 fusion-one-half fusion-column-last 1_2">';
+                        $out .= '<label><b>Días</b></label>';
+                        $out .= '<p><span>' . $dias[$asociado->dias_trabajo] . '</span></p>';
+                        
+                        $out .= '<label><b>Horas</b></label>';
+                        $out .= '<p><span>' . $horarios[$asociado->horarios_trabajo] . '</span></p>';
+                    $out .= '</div>';
+                    
+                    $out .= '<div class="fusion-layout-column fusion_builder_column fusion_builder_column_1_2 fusion-builder-column-7 fusion-one-half fusion-column-last 1_2">';
+                        $out .= '<label><b>País</b></label>';
+                        $out .= '<p><span>' . $asociado->pais->nombre . '</span></p>';
+                        
+                        $out .= '<label><b>Ciudad</b></label>';
+                        $out .= '<p><span>' . $asociado->ciudad->nombre . '</span></p>';
+                    $out .= '</div>'; 
+                
+                $out .= '</div>'; 
+                
+                $out .= '<div class="meta meta-link">';
+                    $out .= '<a class="ver-asociados btn-asociados btn-cancelar" href="javascript:history.back()">Cancelar</a>';
+                    $out .= '<a class="ver-asociados btn-asociados" onclick="contratarAsociado(event)" href="#">Contratar</a>';
+                $out .= '</div>';
+            $out .= '</div>';
+            
+            $out .= '<hr>';
+            
+            $out .= '<div class="comment-panel">';
+                $out .= '<h4>Comentarios</h4>';
+            $out .= '</div>';
+        $out .= '</form>';
+        $out .= '</div>';
+        $out .= '</div>';
+        $out .= '</div>';
+        $out .= '</div>';
+        return $out;
+    }else{
+        //echo null;
+        //$out = '<div class="apilogin-wrapper" id="yn-login"> <form class="login"> <p class="title">Ingreso</p> <span class="error-msg" id="login-error"></span> <input type="text" placeholder="Email" name="api-username" id="api-username" autofocus/> <i class="fa fa-user"></i> <input type="password" name="api-password" id="api-password" placeholder="Contraseña" /> <i class="fa fa-key"></i> <center><p>¿No tienes cuenta?</p></center> <center><a href="https://youneed.com.ec/registro_escoger/">Registrate</a></center> <center><a href="#">¿Olvidaste tu contraseña?</a></center> <button> <i class="spinner"></i> <span class="state">Ingresar</span> </button> </form> <footer></footer> </p> </div>';
+        $out = '<div class="fusion-fullwidth fullwidth-box hundred-percent-fullwidth non-hundred-percent-height-scrolling" style="background-color: #f3f3f3;background-position: center center;background-repeat: no-repeat;padding-top:45px;padding-right:8%;padding-bottom:45px;padding-left:8%;"><div class="fusion-builder-row fusion-row "><div class="fusion-layout-column fusion_builder_column fusion_builder_column_1_1 fusion-builder-column-2 fusion-one-full fusion-column-first fusion-column-last 1_1" style="margin-top:0px;margin-bottom:20px;">';
+        $out .= '<h2>Acceso para usuarios</h2>';
+        $out .= '<p>Por favor inicie sesión antes de continuar.</p>';
+        $out .= '<a class="menu-text fusion-button button-default button-small btn-trigger-login" href="#">Ingresar</a>';
+        $out .= '<h3>¿No tienes cuenta?</h3>';
+        $out .= '<a href="https://youneed.com.ec/registro_escoger/">Registrate</a>';
+        $out .= '</div>';
+        $out .= '</div>';
+        $out .= '</div>';
+        
+        //wp_register_script('show-login', 'https://youneed.com.ec/app/js/showLogin.js', array('jquery'),'1.1', true);
+        //wp_enqueue_script('show-login');
+        return $out;
+    };
+    
+}
+add_shortcode( 'api_youneed_asociado', 'api_youneed_asociado' );
+
+
+/**
+ *
+ * API - YouNeed
+ * FILTROS ASOCIADOS
+ * 
+ */
+function api_youneed_listar_asociados(){
+        $text = "<h3 class='filtro-titulo'><b>Ordenar</b></h3>"; 
+        //$text .= "<span class='filtro'>" . $result->count . ($data > 1 ? " resultados" : " resultado") . "</span>";
+        
+        return $text;
+}
+add_shortcode( 'api_youneed_listar_asociados', 'api_youneed_listar_asociados' );
+
+function api_youneed_filtro_ciudades(){
+        $text = "<h3 class='filtro-titulo'><b>Ubicación</b></h3>"; 
+        //$text .= "<span class='filtro'>" . $result->count . ($data > 1 ? " resultados" : " resultado") . "</span>";
+        
+        return $text;
+}
+add_shortcode( 'api_youneed_filtro_ciudades', 'api_youneed_filtro_ciudades' );
+
+function api_youneed_filtro_categoria(){
+        $text = "<h3 class='filtro-titulo'><b>Categortía</b></h3>"; 
+        //$text .= "<span class='filtro'>" . $result->count . ($data > 1 ? " resultados" : " resultado") . "</span>";
+        
+        return $text;
+}
+add_shortcode( 'api_youneed_filtro_categoria', 'api_youneed_filtro_categoria' );
---
+++ header.php
@@ -0,0 +1,195 @@
+<?php
+header('Access-Control-Allow-Origin: *'); 
+/**
+ * Header template.
+ *
+ * @package Avada
+ * @subpackage Templates
+ */
+
+// Do not allow directly accessing this file.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'Direct script access denied.' );
+}
+?>
+<!DOCTYPE html>
+<html class="<?php avada_the_html_class(); ?>" <?php language_attributes(); ?>>
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	
+	<script>
+	function addEvent(elm, evType, fn) {
+        if (elm.addEventListener) {
+            elm.addEventListener(evType, fn, false);
+            return true;
+        }
+        else if (elm.attachEvent) {
+            var r = elm.attachEvent('on' + evType, fn);
+            return r;
+        }
+        else {
+            elm['on' + evType] = fn;
+        }
+    }
+
+	</script>
+	
+	<?php Avada()->head->the_viewport(); ?>
+
+	<?php wp_head(); ?>
+
+	<?php $object_id = get_queried_object_id(); ?>
+	<?php $c_page_id = Avada()->fusion_library->get_page_id(); ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8.8.5/dist/sweetalert2.all.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@8.8.5/dist/sweetalert2.min.css">
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/css/swiper.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/css/swiper.min.css">
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/js/swiper.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/js/swiper.min.js"></script>
+
+	<script type="text/javascript">
+		var doc = document.documentElement;
+		doc.setAttribute('data-useragent', navigator.userAgent);
+	</script>
+	
+	<script type="text/javascript" src="https://youneed.com.ec/app/js/spin.js'; ?>" ></script>
+	<script type="text/javascript" src="https://youneed.com.ec/app/js/main.js"></script>
+	
+
+	<?php
+	/**
+	 *
+	 * The settings below are not sanitized.
+	 * In order to be able to take advantage of this,
+	 * a user would have to gain access to the database
+	 * in which case this is the least on your worries.
+	 */
+	echo apply_filters( 'avada_google_analytics', Avada()->settings->get( 'google_analytics' ) ); // WPCS: XSS ok.
+	echo apply_filters( 'avada_space_head', Avada()->settings->get( 'space_head' ) ); // WPCS: XSS ok.
+	?>
+</head>
+    <!-- LOGIN -->
+    <div class="apilogin-wrapper" id="yn-login">
+      <form class="login">
+        <p class="title">Ingreso</p>
+        <span class="error-msg" id="login-error"></span>
+        <input type="text" placeholder="Email" name="api-username" id="api-username" autofocus/>
+        <i class="fa fa-user"></i>
+        <input type="password" name="api-password" id="api-password" placeholder="Contraseña" />
+        <i class="fa fa-key"></i>
+        <center><p>¿No tienes cuenta?</p></center>
+        <center><a href="https://youneed.com.ec/registro_escoger/">Registrate</a></center>
+        <center><a href="#">¿Olvidaste tu contraseña?</a></center>
+        <button>
+          <i class="spinner"></i>
+          <span class="state">Ingresar</span>
+        </button>
+      </form>
+      <footer></footer>
+      </p>
+    </div>
+    <!-- END LOGIN -->
+<?php
+$wrapper_class = ( is_page_template( 'blank.php' ) ) ? 'wrapper_blank' : '';
+
+if ( 'modern' === Avada()->settings->get( 'mobile_menu_design' ) ) {
+	$mobile_logo_pos = strtolower( Avada()->settings->get( 'logo_alignment' ) );
+	if ( 'center' === strtolower( Avada()->settings->get( 'logo_alignment' ) ) ) {
+		$mobile_logo_pos = 'left';
+	}
+}
+
+?>
+<script>
+	var elLog = document.querySelector("a[title='login']");
+	
+	addEvent(elLog, "click", function(event){
+         alert('testing');
+        if(event.preventDefault){
+            event.preventDefault;
+        }
+         if (event.stopPropagation) {
+            event.stopPropagation();
+         }
+        return false;
+    });
+	
+    (function($){
+	    jQuery('.login').click(function(e) {
+            e.preventDefault();
+        });
+        
+        
+        
+        jQuery("#yn-login").click(function(event){ 
+            if(event.target.id=="yn-login"){
+                if(jQuery("#yn-login").hasClass("yn-login")){  
+                    jQuery("#yn-login").removeClass("yn-login");
+                } 
+            }
+        });
+    })(jQuery);
+</script>
+<body <?php body_class(); ?>>
+
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'Avada' ); ?></a>
+	<?php
+	do_action( 'avada_before_body_content' );
+
+	$boxed_side_header_right = false;
+	$page_bg_layout          = 'default';
+	if ( $c_page_id && is_numeric( $c_page_id ) ) {
+		$fpo_page_bg_layout = get_post_meta( $c_page_id, 'pyre_page_bg_layout', true );
+		$page_bg_layout     = ( $fpo_page_bg_layout ) ? $fpo_page_bg_layout : $page_bg_layout;
+	}
+
+	?>
+	<?php if ( ( ( 'Boxed' === Avada()->settings->get( 'layout' ) && ( 'default' === $page_bg_layout || '' == $page_bg_layout ) ) || 'boxed' === $page_bg_layout ) && 'Top' != Avada()->settings->get( 'header_position' ) ) : ?>
+		<div id="boxed-wrapper">
+	<?php endif; ?>
+	<?php if ( ( ( 'Boxed' === Avada()->settings->get( 'layout' ) && 'default' === $page_bg_layout ) || 'boxed' === $page_bg_layout ) && 'framed' === Avada()->settings->get( 'scroll_offset' ) ) : ?>
+		<div class="fusion-sides-frame"></div>
+	<?php endif; ?>
+	<div id="wrapper" class="<?php echo esc_attr( $wrapper_class ); ?>">
+		<div id="home" style="position:relative;top:-1px;"></div>
+		<?php avada_header_template( 'Below', ( is_archive() || Avada_Helper::bbp_is_topic_tag() ) && ! ( class_exists( 'WooCommerce' ) && is_shop() ) ); ?>
+		<?php if ( 'Left' === Avada()->settings->get( 'header_position' ) || 'Right' === Avada()->settings->get( 'header_position' ) ) : ?>
+			<?php avada_side_header(); ?>
+		<?php endif; ?>
+
+		<?php avada_sliders_container(); ?>
+
+		<?php avada_header_template( 'Above', ( is_archive() || Avada_Helper::bbp_is_topic_tag() ) && ! ( class_exists( 'WooCommerce' ) && is_shop() ) ); ?>
+
+		<?php if ( has_action( 'avada_override_current_page_title_bar' ) ) : ?>
+			<?php do_action( 'avada_override_current_page_title_bar', $c_page_id ); ?>
+		<?php else : ?>
+			<?php avada_current_page_title_bar( $c_page_id ); ?>
+		<?php endif; ?>
+		<?php do_action( 'avada_after_page_title_bar' ); ?>
+
+		<?php
+		$main_css   = '';
+		$row_css    = '';
+		$main_class = '';
+
+		if ( apply_filters( 'fusion_is_hundred_percent_template', false, $c_page_id ) ) {
+			$main_css         = 'padding-left:0px;padding-right:0px;';
+			$hundredp_padding = get_post_meta( $c_page_id, 'pyre_hundredp_padding', true );
+			if ( Avada()->settings->get( 'hundredp_padding' ) && ! $hundredp_padding ) {
+				$main_css = 'padding-left:' . Avada()->settings->get( 'hundredp_padding' ) . ';padding-right:' . Avada()->settings->get( 'hundredp_padding' );
+			}
+			if ( $hundredp_padding ) {
+				$main_css = 'padding-left:' . $hundredp_padding . ';padding-right:' . $hundredp_padding;
+			}
+			$row_css    = 'max-width:100%;';
+			$main_class = 'width-100';
+		}
+		do_action( 'avada_before_main_container' );
+		?>
+		<main id="main" class="clearfix <?php echo esc_attr( $main_class ); ?>" style="<?php echo esc_attr( $main_css ); ?>">
+			<div class="fusion-row" style="<?php echo esc_attr( $row_css ); ?>">
