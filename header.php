83d79444b4da8af4897d92a6b878a45ab0fd8b4f
dibuchis <dibuchis@gmail.com>
2019-05-27 17:16:51 -0500

    Checkout

--- header.php
+++ header.php
@@ -18,13 +18,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	
-	<link href="lib/air-datepicker/css/datepicker.min.css" rel="stylesheet" type="text/css">
-	<script src="lib/air-datepicker/js/datepicker.min.js"></script>
+	<link href="<?php get_template_directory_uri(); ?>/lib/air-datepicker/css/datepicker.min.css" rel="stylesheet" type="text/css">
+	<script src="<?php get_template_directory_uri(); ?>/lib/air-datepicker/js/datepicker.min.js"></script>
 
 	<!-- Include English language -->
-	<script src="lib/air-datepicker/js/i18n/datepicker.en.js"></script>
+	<script src="<?php get_template_directory_uri(); ?>lib/air-datepicker/js/i18n/datepicker.en.js"></script>
 	
-	<script src="lib/main.js"></script>
+	<script src="<?php get_template_directory_uri(); ?>lib/main.js"></script>
 
 	
 	<script>
