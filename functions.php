e0b4521efdee46dc9ce9c1a37d7da51f6766b8ff
dibuchis <dibuchis@gmail.com>
2019-05-27 21:44:24 -0500

    Datepicker

--- functions.php
+++ functions.php
@@ -545,8 +545,8 @@ function api_youneed_contratar(){
 
             $out .= '<table class="table-2">';
                 $out .= '<tr>';
-                $out .= '<td><div class="container"><div class="col-sm-6"><div class="form-group"><div class="input-group date" id="datetimepicker12"><input type="text" class="form-control" /><span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span></div></div></div>';
-                $out .= '<script>jQuery("#datetimepicker12").datetimepicker({ inline: true, sideBySide: true });</script>';
+                $out .= '<td><div style="overflow:hidden;"><div class="form-group"><div class="row"><div class="col-md-8"><div id="datetimepicker12"></div></div></div></div>';
+                $out .= '<script>//jQuery("#datetimepicker12").datetimepicker({ inline: true, sideBySide: true });</script>';
                 $out .= '</div></td>';
                 $out .= '<td><label>Fecha del servicio</label><input id="Pedido[fecha_para_servicio]" type="text" id="fecha_servicio" name="fecha_servicio" ><div id="fecha_servicio_content"></div></td>';
                 $out .= '</tr>';
