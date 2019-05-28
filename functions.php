6e85c204f3045fd4175688bb79d851c982ea8d17
dibuchis <dibuchis@gmail.com>
2019-05-28 01:09:02 -0500

    Datepicker

--- functions.php
+++ functions.php
@@ -541,8 +541,8 @@ function api_youneed_contratar(){
         $out .= '<div class="fusion-layout-column fusion_builder_column fusion_builder_column_1_1 fusion-builder-column-2 fusion-one-full fusion-column-first fusion-column-last 1_1" style="margin-top:0px;margin-bottom:20px;">';
         
         $out .= '<form id="contratar-asociado" method="post" action="https://youneed.com.ec/contratar/" >';
-        $out .= '<hr style="overflow:hidden;margin-bottom:35px;">';
-        $out .= '<h2>Checkout</h2>';
+        // $out .= '<hr style="overflow:hidden;margin-bottom:35px;">';
+        $out .= '<h2><center>Checkout</center></h2>';
 			//$out .= '<input type="hidden" name="_csrf" value="XDB8ErUw8zD_28OF8uOJGeVszR7GuztlpYlXhhaPVNYTWDlcgFW_QZmR7rynishGig2scrH4Yg_20RnBL7cVsg==">';
             
                 $out .= '<input id="asociado_id" type="hidden" name="Pedido[asociado_id]" value="' . $asociado_id . '">';
