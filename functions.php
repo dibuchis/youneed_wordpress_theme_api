d8dcfb15de46f49a08f4fad6951f263544684c7b
dibuchis <dibuchis@gmail.com>
2019-05-27 22:07:43 -0500

    Datepicker

--- functions.php
+++ functions.php
@@ -466,7 +466,7 @@ function api_youneed_contratar(){
     // wp_register_style('bootstrap-icons', 'https://youneed.com.ec/wp-content/themes/Avada-Child-Theme/lib/css/bootstrap.min.css');
     // wp_enqueue_style( 'bootstrap' );
 
-    wp_register_style('bootstrap-icons', 'https://youneed.com.ec/wp-content/themes/Avada-Child-Theme/lib/glyphicons-only-bootstrap/bootstrap.min.css');
+    wp_register_style('bootstrap-icons', 'https://youneed.com.ec/wp-content/themes/Avada-Child-Theme/lib/glyphicons-only-bootstrap/css/bootstrap.min.css');
     wp_enqueue_style( 'bootstrap-icons' );
 
     $user = null;
